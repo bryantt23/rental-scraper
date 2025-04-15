b14f5b2b5b406359f6fe8ccb0ab34ed36dc17a98
bryant tunbutr <bryantt23@users.noreply.github.com>
2025-04-15 20:26:29 +0800

    can output to file

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 {
+    const input = document.createElement('input')
+    input.type = 'file'
+    input.accept = '.txt'
+
+    let listingData = ``
+
     const delay = (delayTime) => {
         return new Promise(resolve => {
             setTimeout(resolve, delayTime)
         })
+    }
+
+    const updateFile = () => {
+        console.log("🚀 ~ updateFile ~ listingData:", listingData)
+        const blob = new Blob([listingData], { type: 'text/plain' })
+        const url = URL.createObjectURL(blob)
+        const a = document.createElement('a')
+        a.href = url
+        a.download = 'listings.txt'
+        document.body.appendChild(a)
+        a.click()
+        document.body.removeChild(a)
+        URL.revokeObjectURL(url)// Clean up the object URL
     }
 
     const getListings = () => {
@@ -12,20 +31,25 @@
             const tds = listing.querySelectorAll("td")
             const arr = Array.from(tds)
             const listingDescription = arr[0].innerText
-            console.log(`Description: ${listingDescription} - URL: ${href}`)
+            listingData += `\nDescription: ${listingDescription}URL: ${href}`
         }
     }
 
-    const goToNextPage = async () => {
+    const visitPages = async () => {
         let nextPageButton = document.querySelector("#btn-page-next")
         // while (!nextPageButton.disabled) {
-        await delay(2000)
         getListings()
+        await delay(2000)
         // debugger
         // nextPageButton.click()
         // nextPageButton = document.querySelector("#btn-page-next")
         // }
     }
 
-    goToNextPage()
+    const start = async () => {
+        visitPages()
+        updateFile()
+    }
+
+    start()
 }
