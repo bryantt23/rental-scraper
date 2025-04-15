b576e9bd76e626d38acf971167af802685f7987f
bryant tunbutr <bryantt23@users.noreply.github.com>
2025-04-15 19:40:38 +0800

    get description and url

--- index.js
+++ index.js
@@ -5,13 +5,26 @@
         })
     }
 
+    const getListings = () => {
+        const listings = document.querySelectorAll(".listing-holder")
+        for (const listing of listings) {
+            const href = listing.querySelector("a").href
+            const tds = listing.querySelectorAll("td")
+            const arr = Array.from(tds)
+            const listingDescription = arr[0].innerText
+            console.log(`Description: ${listingDescription} - URL: ${href}`)
+        }
+    }
+
     const goToNextPage = async () => {
         let nextPageButton = document.querySelector("#btn-page-next")
-        while (!nextPageButton.disabled) {
-            nextPageButton.click()
-            await delay(2000)
-            nextPageButton = document.querySelector("#btn-page-next")
-        }
+        // while (!nextPageButton.disabled) {
+        await delay(2000)
+        getListings()
+        // debugger
+        // nextPageButton.click()
+        // nextPageButton = document.querySelector("#btn-page-next")
+        // }
     }
 
     goToNextPage()
